9f8305db830593f10a5bb1995da7d52f34a31f8e
Christof Van Cauteren <christofvc@gmail.com>
2017-01-12 23:25:44 +0100

    Initial commit
    And so it begins…

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CommentController extends Controller
+{
+    /**
+     * Send back all comments as JSON
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return response()->json(Comment::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $comment = new Comment;
+
+        $comment->author = $request->author;
+        $comment->text = $request->text;
+
+        return response()->json(array('success' => true));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Comment::destroy($id);
+
+        return response()->json(array('success' => true));
+    }
+}
